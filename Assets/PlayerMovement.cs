fe5af383c6375473d20d70556bf57c66fbd394ce
Cheikh Tidiane DIEDHIOU <cheikht2011@gmail.com>
2020-01-08 00:29:51 +0100

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    
+    public float forwadForce = 2000f;
+
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         rb.AddForce(0, 0, forwadForce * Time.deltaTime);
+
+         if ( Input.GetKey("d") )
+         {
+             rb.AddForce(sidewaysForce * Time.deltaTime, 0,  0, ForceMode.VelocityChange);
+         }
+
+         if ( Input.GetKey("a") )
+         {
+             rb.AddForce(-sidewaysForce * Time.deltaTime, 0,  0, ForceMode.VelocityChange);
+         }
+    }
+}
